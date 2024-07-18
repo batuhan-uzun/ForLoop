fef70b85bc4923d3d3b3d8f9a2ce4be2409d142f
batuhan-uzun <batuhanuzun2356@gmail.com>
2024-07-18 17:28:33 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿for (int i = 0; i < 10; i++)
+{
+    Console.WriteLine("Kendime inanıyorum, ben bu yazılım işini hallederim!");
+}
+Console.WriteLine("");
+
+for (int i = 2;i < 20; i++)
+{
+    Console.Write(i + " ");
+}
+Console.WriteLine("\n");
+
+for (int i = 2; i < 20; i++)
+{
+    if (i % 2 == 0) Console.Write(i + " ");
+}
+Console.WriteLine("\n");
+
+int sum = 0;
+for (int i = 51; i < 150; i++)
+{
+    sum += i;
+}
+Console.WriteLine(sum + "\n");
+
+int evenSum = 0;
+int oddSum = 0;
+
+for (int i = 2; i < 120; i++)
+{
+    if (i % 2 == 0) evenSum+=i;
+    else oddSum+=i;
+}
+
+Console.WriteLine("1 ile 120 arasındaki çift sayıların toplamı: "+ evenSum + "\n" + "1 ile 120 arasındaki tek sayıların toplamı: " + oddSum);
